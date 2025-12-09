a89d8ac226327a2c76e0b786c9f3f70a350a38db
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-12-09 10:56:11 +0900

    [chore] example Script

--- Assets/Scripts/NewScript1.cpp
+++ Assets/Scripts/NewScript1.cpp
@@ -1,17 +1,17 @@
-#include "NewScript.h"
+#include "NewScript1.h"
 #include "Core/World.h"
 
 namespace Alice
 {
     // 이 스크립트를 리플렉션/팩토리 시스템에 등록합니다.
-    REGISTER_SCRIPT(NewScript);
+    REGISTER_SCRIPT(NewScript1);
 
-    void NewScript::OnCreate(World& world, EntityId entity)
+    void NewScript1::OnCreate(World& world, EntityId entity)
     {
         // 초기화 로직을 여기에 작성하세요.
     }
 
-    void NewScript::OnUpdate(World& world, EntityId entity, float deltaTime)
+    void NewScript1::OnUpdate(World& world, EntityId entity, float deltaTime)
     {
         // 매 프레임 호출되는 로직을 여기에 작성하세요.
     }
