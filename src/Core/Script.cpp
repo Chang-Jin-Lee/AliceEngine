0946f74a6e8dea58d3ee401927a9c0760dd44c5f
Chang-Jin-Lee <ckdwls525@gmail.com>
2026-01-15 11:54:59 +0900

    [fix] 스크립트 빌드 에러나던 부분 수정

--- src/Core/Script.cpp
+++
@@ -1,7 +0,0 @@
-// 통합 소스: 하위 호환성을 위해 유지됩니다.
-// 실제 구현은 다음 파일들에 분리되어 있습니다:
-// - Core/IScript.cpp
-// - Core/ScriptFactory.cpp
-// - Core/ScriptSystem.cpp
-//
-// CMakeLists.txt에서 각각을 직접 컴파일하므로 이 파일은 빈 파일입니다.
--- src/Core/Script.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// 통합 헤더: 하위 호환성을 위해 모든 스크립트 관련 헤더를 포함합니다.
-// 새로운 코드에서는 개별 헤더를 직접 include하는 것을 권장합니다.
-
-#include "Core/IScript.h"
-#include "Core/ScriptFactory.h"
-#include "Core/ScriptSystem.h"
-#include "Components/ScriptComponent.h"
