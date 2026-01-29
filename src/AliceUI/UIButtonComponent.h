c6f058a5b1d13181cfda5ef607766d2965e0c739
Chang-Jin-Lee <ckdwls525@gmail.com>
2026-01-29 23:58:48 +0900

    [fix] 안전하게 쓰도록 예외처리

--- src/AliceUI/UIButtonComponent.h
+++ src/AliceUI/UIButtonComponent.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <algorithm>
 #include <vector>
 #include <functional>
 #include <DirectXMath.h>
@@ -29,18 +30,40 @@ namespace Alice
 
 		// 이벤트 델리게이트 (스크립트에서 등록)
 		using ButtonDelegate = std::function<void()>;
-		std::vector<ButtonDelegate> onPressed;
-		std::vector<ButtonDelegate> onReleased;
-		std::vector<ButtonDelegate> onHovered;
+		struct ButtonDelegateEntry
+		{
+			ButtonDelegate fn;
+			std::function<bool()> isValid;
+			bool invalid{ false };
+		};
+		std::vector<ButtonDelegateEntry> onPressed;
+		std::vector<ButtonDelegateEntry> onReleased;
+		std::vector<ButtonDelegateEntry> onHovered;
 
-		void AddOnPressed(ButtonDelegate fn) { if (fn) onPressed.push_back(std::move(fn)); }
-		void AddOnReleased(ButtonDelegate fn) { if (fn) onReleased.push_back(std::move(fn)); }
-		void AddOnHovered(ButtonDelegate fn) { if (fn) onHovered.push_back(std::move(fn)); }
+		void AddOnPressed(ButtonDelegate fn) { if (fn) onPressed.push_back({ std::move(fn), {}, false }); }
+		void AddOnReleased(ButtonDelegate fn) { if (fn) onReleased.push_back({ std::move(fn), {}, false }); }
+		void AddOnHovered(ButtonDelegate fn) { if (fn) onHovered.push_back({ std::move(fn), {}, false }); }
+		void AddOnPressedSafe(ButtonDelegate fn, std::function<bool()> isValid) { if (fn) onPressed.push_back({ std::move(fn), std::move(isValid), false }); }
+		void AddOnReleasedSafe(ButtonDelegate fn, std::function<bool()> isValid) { if (fn) onReleased.push_back({ std::move(fn), std::move(isValid), false }); }
+		void AddOnHoveredSafe(ButtonDelegate fn, std::function<bool()> isValid) { if (fn) onHovered.push_back({ std::move(fn), std::move(isValid), false }); }
 		void ClearDelegates()
 		{
 			onPressed.clear();
 			onReleased.clear();
 			onHovered.clear();
+		}
+		void CullInvalidDelegates()
+		{
+			auto Cull = [](auto& list)
+			{
+				list.erase(std::remove_if(list.begin(), list.end(), [](const ButtonDelegateEntry& entry)
+				{
+					return entry.invalid;
+				}), list.end());
+			};
+			Cull(onPressed);
+			Cull(onReleased);
+			Cull(onHovered);
 		}
 
 		bool ConsumeClick()
